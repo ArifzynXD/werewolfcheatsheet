1ac21a135f2b4fb0f7bbb438e8a32e678f9d65ad
Martyn Ranyard <ranyardm@gmail.com>
2013-11-16 20:15:39 +0000

    Lover 1 works

--- app/view/PlayerRoleListItem.js
+++ app/view/PlayerRoleListItem.js
@@ -1,45 +1,140 @@
 
 Ext.define('TouchWolf.view.PlayerRoleListItem', {
     extend: 'Ext.dataview.component.DataItem',
-    requires: ['Ext.Button'],
+    requires: ['Ext.Button', 'Ext.Spacer', 'Ext.field.Select'],
     xtype: 'playerrolelistitem',
 
     config: {
-        padding: 10,
+        padding: 0,
         layout: {
             type: 'hbox'
         },
         defaults: {
-            margin: 5
+            margin: 0
         },
         items: [{
             xtype: 'label',
             flex: 1,
             html: 'name',
-            itemId: 'textCmp'
+            itemId: 'textCmp',
+            padding: 10,
+            width: 5
         },{
-            xtype: 'button',
-            itemId: 'btn',
-            text: 'remove',
+            xtype: 'selectfield',
+            itemId: 'role1',
+            options: [
+                {text: 'Villager', value: 'Villager'},
+                {text: 'Werewolf', value: 'Werewolf'},
+                {text: 'Seer', value: 'Seer'},
+                {text: 'Healer', value: 'Healer'},
+                {text: 'Hunter', value: 'Hunter'},
+                {text: 'Witch', value: 'Witch'},
+                {text: 'Little Girl', value: 'Little Girl'},
+                {text: 'Cupid', value: 'Cupid'}
+            ],
+            displayField: 'text',
+            valueField: 'value',
             listeners: {
-                tap: function (me) {
-                    Ext.getStore('playerStore').remove(me.getData().record);
+                'change': function(me,newValue,oldValue,eOpts) {
+                        rec = me.getData().record;
+                        rec.set('role1',newValue);
+                        Ext.getStore('playerStore').sync();
+                }
+            }
+        },{
+            xtype: 'spacer',
+            flex: 1
+        },{
+            xtype: 'selectfield',
+            itemId: 'role2',
+            options: [
+                {text: 'Villager', value: 'Villager'},
+                {text: 'Werewolf', value: 'Werewolf'},
+                {text: 'Seer', value: 'Seer'},
+                {text: 'Healer', value: 'Healer'},
+                {text: 'Hunter', value: 'Hunter'},
+                {text: 'Witch', value: 'Witch'},
+                {text: 'Little Girl', value: 'Little Girl'},
+                {text: 'Cupid', value: 'Cupid'}
+            ],
+            displayField: 'text',
+            valueField: 'value',
+            listeners: {
+                'change': function(me,newValue,oldValue,eOpts) {
+                    rec = me.getData().record;
+                    rec.set('role2',newValue);
                     Ext.getStore('playerStore').sync();
                 }
             }
+        },{
+            xtype: 'spacer',
+            flex: 1
+        },{
+            xtype: 'selectfield',
+            itemId: 'lover1',
+            label: 'Lover1',
+            options: [
+                {text: 'Noone', value: 'Noone'}
+            ],
+            displayField: 'text',
+            valueField: 'value',
+            listeners: {
+                'change': function(me,newValue,oldValue,eOpts) {
+                    rec = me.getData().record;
+                    rec.set('lover1',newValue);
+                    Ext.getStore('playerStore').sync();
+                }
+            }
+        },{
+            xtype: 'spacer',
+            flex: 1
         }]
     },updateRecord: function(record) {
         me = this;
 
-        data = me.down('#btn').getData();
+        data = me.down('#role1').getData();
         if (data == null) {
             data = {record : record}
         } else {
             data.record = record;
         }
         me.down('#textCmp').setHtml(record.get('name'));
-        me.down('#textCmp').setData(data);
-        me.down('#btn').setData(data);
+        role1field = me.down('#role1');
+        if (role1field !== null) {
+            if (role1field.getData !== data) {
+                role1field.setData(data);
+            }
+            if (role1field.getValue() !== record.get('role1')) {
+                role1field.setValue(record.get('role1'));
+            }
+        }
+        role2field = me.down('#role2');
+        if (role2field !== null) {
+            if (role2field.getData !== data) {
+                role2field.setData(data);
+            }
+            if (role2field.getValue() !== record.get('role2')) {
+                role2field.setValue(record.get('role2'));
+            }
+        }
+        store = Ext.getStore('playerStore');
+        lovers = [{text:'Noone',value:null}];
+        store.each(function (item, index, length) {
+            lovers.push({text:item.data.name,value:item.data.name});
+        });
+        console.log(lovers);
+        lover1field = me.down('#lover1');
+        if (lover1field !== null) {
+            if (lover1field.getData !== data) {
+                lover1field.setData(data);
+            }
+            if (lover1field.getValue() !== record.get('lover1')) {
+                lover1field.setValue(record.get('lover1'));
+            }
+            if (lover1field.getOptions !== lovers) {
+                lover1field.setOptions(lovers);
+            }
+        }
 
         me.callParent(arguments);
     }
